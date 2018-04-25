0adf275e6327cd85e0971e71b588fb13cddb935d
Nikita Sinyaev <sinyaev.nikita@gmail.com>
2018-04-25 17:56:03 +0300

    Fix errors. Add timer. Add more error-checking,

--- NivalTest/CalculationDeserializer.cs
+++ NivalTest/CalculationDeserializer.cs
@@ -24,39 +24,80 @@ namespace NivalTest
                     XmlNode calculation = xmlDoc.DocumentElement.ChildNodes[i];
                     if (calculation.ChildNodes.Count != 3)
                     {
-                        throw new FormatException("Calculation must have 3 child nodes (uid, operand and mod)");
+                        throw new FormatException("Calculation must have 3 child nodes (uid, operand and mod).");
                     }
                     Guid uid = Guid.Empty;
                     Operand operand = Operand.Add;
                     int mod = 0;
+                    bool uidInit = false;
+                    bool opInit = false;
+                    bool modInit = false;
                     foreach (XmlNode item in calculation.ChildNodes)
                     {
                         var value = item.Attributes["value"].Value;
                         switch (item.Attributes["name"].Value)
                         {
                             case "uid":
-                                uid = Guid.Parse(value);
+                                if (!uidInit)
+                                {
+                                    uid = Guid.Parse(value);
+                                    uidInit = true;
+                                }
+                                else
+                                {
+                                    throw new FormatException($"Uid provided more than once.");
+                                }
                                 break;
                             case "operand":
-                                operand = (Operand)Enum.Parse(typeof(Operand), value, true);
+                                if (!opInit)
+                                {
+                                    operand = (Operand)Enum.Parse(typeof(Operand), value, true);
+                                    opInit = true;
+                                }
+                                else
+                                {
+                                    throw new FormatException($"Operand provided more than once.");
+                                }
+                                
                                 break;
                             case "mod":
-                                mod = int.Parse(value);
+                                if (!modInit)
+                                {
+                                    mod = int.Parse(value);
+                                    modInit = true;
+                                }
+                                else
+                                {
+                                    throw new FormatException($"Mod provided more than once.");
+                                }                                
                                 break;
                             default:
-                                throw new FormatException($"Unrecognized property: {item.Attributes["name"].Value}");
+                                throw new FormatException($"Unrecognized property: {item.Attributes["name"].Value}.");
                         }
+                    }
+
+                    if (!uidInit)
+                    {
+                        throw new FormatException($"Uid not provided.");
+                    }
+                    if (!opInit)
+                    {
+                        throw new FormatException($"Operand not provided.");
+                    }
+                    if (!modInit)
+                    {
+                        throw new FormatException($"Mod not provided.");
                     }
 
                     results.Add(new Operation(uid, operand, mod));
                 }
                 catch (FormatException fe)
                 {
-                    Console.WriteLine($"{path}: Error: {fe.Message}, ignoring calculation element");
+                    Console.WriteLine($"{path}: Error: {fe.Message} Ignoring calculation element.");
                 }
                 catch (ArgumentException ae)
                 {
-                    Console.WriteLine($"{path}: Error: unrecognized operation");
+                    Console.WriteLine($"{path}: Error: unrecognized operation. {ae.Message} Ignoring calculation element.");
                 }
             }
 
--- NivalTest/Program.cs
+++ NivalTest/Program.cs
@@ -10,6 +10,11 @@ namespace NivalTest
 {
     class Program
     {
+        //thread-protected by longestFilename
+        static int longest = 0;
+        //thread-protected by longestFilename
+        static string longestFilename = "";
+
         static void MyTask(string filename)
         {
             try
@@ -18,6 +23,17 @@ namespace NivalTest
                 var operations = p.Deserialize(filename);
                 var calc = new Calculator();
                 var result = calc.Calculate(operations);
+                var length = operations.Count;
+
+                lock(longestFilename)
+                {
+                    if (length > longest)
+                    {
+                        longest = length;
+                        longestFilename = filename;
+                    }
+                }
+
                 Console.WriteLine($"{filename} {result}");
             }
             catch (XmlException xe)
@@ -26,22 +42,31 @@ namespace NivalTest
             }
             catch (DivideByZeroException ze)
             {
-                Console.WriteLine($"{filename}: Division By Zero");
+                Console.WriteLine($"{filename}: Division By Zero.");
             }
-        }
+        }        
 
         public static void Main(String[] args)
         {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Error! Expected one argument: path to dir.");
+                Console.WriteLine($"Program execution time: {watch.ElapsedMilliseconds} ms.");
+                Environment.Exit(1);
+            }
+
             string pathToDir = args[0];
 
             if (!Directory.Exists(pathToDir))
             {
-                Console.WriteLine("Invalid path to directory");
+                Console.WriteLine("Invalid path to directory.");
+                Console.WriteLine($"Program execution time: {watch.ElapsedMilliseconds} ms.");
                 Environment.Exit(1);
             }
 
             var files = Directory.EnumerateFiles(pathToDir, "*.xml");
-
             List<Task> tasks = new List<Task>();
 
             foreach (var file in files)
@@ -51,9 +76,14 @@ namespace NivalTest
 
             Task.WaitAll(tasks.ToArray());
 
-            Console.WriteLine("All tasks completed");
+            lock (longestFilename)
+            {
+                Console.WriteLine($"Most successfully deserialized elements in {longestFilename}.");
+            }
 
-            Console.ReadKey();
+            watch.Stop();            
+
+            Console.WriteLine($"Program execution time: {watch.ElapsedMilliseconds} ms.");            
         }        
     }
 }
