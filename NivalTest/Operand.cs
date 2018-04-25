cacfee01f32f09bdb61accf24c224fcc076ee332
Nikita Sinyaev <sinyaev.nikita@gmail.com>
2018-04-25 18:04:27 +0300

    Remove and sort usings.

--- NivalTest/Operand.cs
+++ NivalTest/Operand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NivalTest
+﻿namespace NivalTest
 {
     enum Operand { Add, Subtract, Multiply, Divide }
 }
