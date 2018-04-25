f03c676e8aa3ed5acafd337d82c82550d8dba774
Nikita Sinyaev <sinyaev.nikita@gmail.com>
2018-04-25 18:25:13 +0300

    Refactor Deserialize.

--- NivalTest/CalculationDeserializer.cs
+++ NivalTest/CalculationDeserializer.cs
@@ -6,6 +6,78 @@ namespace NivalTest
 {
     class CalculationDeserializer
     {
+        private Operation DeserializeElement(XmlNode calculation)
+        {            
+            if (calculation.ChildNodes.Count != 3)
+            {
+                throw new FormatException("Calculation must have 3 child nodes (uid, operand and mod).");
+            }
+            Guid uid = Guid.Empty;
+            Operand operand = Operand.Add;
+            int mod = 0;
+            bool uidInit = false;
+            bool opInit = false;
+            bool modInit = false;
+            foreach (XmlNode item in calculation.ChildNodes)
+            {
+                var value = item.Attributes["value"].Value;
+                switch (item.Attributes["name"].Value)
+                {
+                    case "uid":
+                        if (!uidInit)
+                        {
+                            uid = Guid.Parse(value);
+                            uidInit = true;
+                        }
+                        else
+                        {
+                            throw new FormatException($"Uid provided more than once.");
+                        }
+                        break;
+                    case "operand":
+                        if (!opInit)
+                        {
+                            operand = (Operand)Enum.Parse(typeof(Operand), value, true);
+                            opInit = true;
+                        }
+                        else
+                        {
+                            throw new FormatException($"Operand provided more than once.");
+                        }
+
+                        break;
+                    case "mod":
+                        if (!modInit)
+                        {
+                            mod = int.Parse(value);
+                            modInit = true;
+                        }
+                        else
+                        {
+                            throw new FormatException($"Mod provided more than once.");
+                        }
+                        break;
+                    default:
+                        throw new FormatException($"Unrecognized property: {item.Attributes["name"].Value}.");
+                }
+            }
+
+            if (!uidInit)
+            {
+                throw new FormatException($"Uid not provided.");
+            }
+            if (!opInit)
+            {
+                throw new FormatException($"Operand not provided.");
+            }
+            if (!modInit)
+            {
+                throw new FormatException($"Mod not provided.");
+            }
+
+            return new Operation(uid, operand, mod);
+        }
+
         public List<Operation> Deserialize(string path)
         {            
             XmlDocument xmlDoc = new XmlDocument();
@@ -19,74 +91,8 @@ namespace NivalTest
                 try
                 {
                     XmlNode calculation = xmlDoc.DocumentElement.ChildNodes[i];
-                    if (calculation.ChildNodes.Count != 3)
-                    {
-                        throw new FormatException("Calculation must have 3 child nodes (uid, operand and mod).");
-                    }
-                    Guid uid = Guid.Empty;
-                    Operand operand = Operand.Add;
-                    int mod = 0;
-                    bool uidInit = false;
-                    bool opInit = false;
-                    bool modInit = false;
-                    foreach (XmlNode item in calculation.ChildNodes)
-                    {
-                        var value = item.Attributes["value"].Value;
-                        switch (item.Attributes["name"].Value)
-                        {
-                            case "uid":
-                                if (!uidInit)
-                                {
-                                    uid = Guid.Parse(value);
-                                    uidInit = true;
-                                }
-                                else
-                                {
-                                    throw new FormatException($"Uid provided more than once.");
-                                }
-                                break;
-                            case "operand":
-                                if (!opInit)
-                                {
-                                    operand = (Operand)Enum.Parse(typeof(Operand), value, true);
-                                    opInit = true;
-                                }
-                                else
-                                {
-                                    throw new FormatException($"Operand provided more than once.");
-                                }
-                                
-                                break;
-                            case "mod":
-                                if (!modInit)
-                                {
-                                    mod = int.Parse(value);
-                                    modInit = true;
-                                }
-                                else
-                                {
-                                    throw new FormatException($"Mod provided more than once.");
-                                }                                
-                                break;
-                            default:
-                                throw new FormatException($"Unrecognized property: {item.Attributes["name"].Value}.");
-                        }
-                    }
-
-                    if (!uidInit)
-                    {
-                        throw new FormatException($"Uid not provided.");
-                    }
-                    if (!opInit)
-                    {
-                        throw new FormatException($"Operand not provided.");
-                    }
-                    if (!modInit)
-                    {
-                        throw new FormatException($"Mod not provided.");
-                    }
-
-                    results.Add(new Operation(uid, operand, mod));
+                    var res = DeserializeElement(calculation);
+                    results.Add(res);
                 }
                 catch (FormatException fe)
                 {
